a89ce7cd67f375e3669319d668f66da2dac88433
henteko <henteko07@gmail.com>
2014-08-20 12:44:04 +0900

    NSUserNotificationCenterを表示するようにした

--- Zoi/AppDelegate.swift
+++ Zoi/AppDelegate.swift
@@ -17,16 +17,37 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         var zois = NSArray(contentsOfFile: filepath) as NSArray
         var idx = Int(arc4random_uniform(UInt32(zois.count)))
         var picpath = zois[idx]["image"] as String?
+        var word = zois[idx]["word"] as String?
         var pastbord = NSPasteboard.generalPasteboard()
         pastbord.declareTypes([NSStringPboardType], owner: nil)
         pastbord.setString(picpath, forType: NSStringPboardType)
-        NSApplication.sharedApplication().terminate(self)
+
+        showUserNotification(picpath!, zoiWord: word!)
+
+         NSApplication.sharedApplication().terminate(self)
     }
 
     func applicationWillTerminate(aNotification: NSNotification?) {
         // Insert code here to tear down your application
     }
 
+    func showUserNotification(imagePath:String, zoiWord:String) {
+        var imageURL: NSURL = NSURL.URLWithString(imagePath)
+        var imageData: NSData = NSData(contentsOfURL: imageURL)
+        var image: NSImage = NSImage(data: imageData)
+
+        var notification:NSUserNotification = NSUserNotification()
+        notification.title = "zoi-app"
+        notification.subtitle = zoiWord
+        notification.informativeText = imagePath
+        notification.contentImage = image
+
+        notification.soundName = NSUserNotificationDefaultSoundName
+        var notificationcenter:NSUserNotificationCenter = NSUserNotificationCenter.defaultUserNotificationCenter()
+        if let notificationCenter = NSUserNotificationCenter.defaultUserNotificationCenter() {
+            notificationcenter.scheduleNotification(notification)
+        }
+    }
 
 }
 
